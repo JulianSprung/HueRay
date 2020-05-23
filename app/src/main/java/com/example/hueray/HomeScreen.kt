c97a46b1024f8dbc822aebd64a62bf86387c80eb
anli66 <a.j.lisowska@gmail.com>
2020-05-23 14:34:30 +0100

    added event screen

--- app/src/main/java/com/example/hueray/HomeScreen.kt
+++ app/src/main/java/com/example/hueray/HomeScreen.kt
@@ -9,10 +9,7 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home_screen.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,17 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,30 +36,15 @@ class HomeScreen : Fragment() {
             findNavController().navigate(R.id.action_homeScreen_to_dailyPleasureAndPurpose)
         }
 
+        // if the add event button is pressed we navigate to screen to free text about an event
+        button_event.setOnClickListener {
+            findNavController().navigate(R.id.action_homeScreen_to_addEvent)
+        }
+
         // If the show statics button is pressed we navigate to our old thanks for rating screen
         // which shows the last values.
         button_stats.setOnClickListener {
             findNavController().navigate(R.id.action_homeScreen_to_thanks_for_rating)
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
